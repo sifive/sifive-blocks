2ab9589bcbbcdc191a6e78de41d4b569c8b1dd9a
Megan Wachs <megan@sifive.com>
2018-04-19 17:16:54 -0700

    RTC: Add register descriptions
    RTC: use Timer trait and MultiIOModule
    
    RTC: handle Vec[Bool] instead of UInt for ip

--- src/main/scala/devices/mockaon/RTC.scala
+++ src/main/scala/devices/mockaon/RTC.scala
@@ -3,19 +3,36 @@ package sifive.blocks.devices.mockaon
 
 import Chisel._
 import Chisel.ImplicitConversions._
+import chisel3.experimental.MultiIOModule
 import freechips.rocketchip.util.AsyncResetReg
+import freechips.rocketchip.regmapper.RegFieldDesc
 
-import sifive.blocks.util.{SlaveRegIF, GenericTimer}
+import sifive.blocks.util.{SlaveRegIF, GenericTimer, GenericTimerIO, DefaultGenericTimerCfgDescs}
 
+class RTC extends MultiIOModule with GenericTimer {
 
-class RTC extends GenericTimer {
+  protected def prefix = "rtc"
   protected def countWidth = 48
   protected def cmpWidth = 32
   protected def ncmp = 1
   protected def countEn = countAlways
-  override protected lazy val ip = Reg(next = elapsed(0))
+  override protected lazy val ip = RegNext(elapsed)
   override protected lazy val zerocmp = Bool(false)
-  protected lazy val countAlways = AsyncResetReg(io.regs.cfg.write.countAlwayys, io.regs.cfg.write_countalways && unlocked)(0)
+  protected lazy val countAlways = AsyncResetReg(io.regs.cfg.write.countAlways, io.regs.cfg.write_countAlways && unlocked)(0)
   protected lazy val feed = Bool(false)
-  lazy val io = new GenericTimerIO
+
+  override protected lazy val feed_desc = RegFieldDesc.reserved
+  override protected lazy val key_desc = RegFieldDesc.reserved
+  override protected lazy val cfg_desc = DefaultGenericTimerCfgDescs("rtc", ncmp).copy(
+    sticky = RegFieldDesc.reserved,
+    zerocmp = RegFieldDesc.reserved,
+    deglitch = RegFieldDesc.reserved,
+    running = RegFieldDesc.reserved,
+    center = Seq.fill(ncmp){ RegFieldDesc.reserved },
+    extra = Seq.fill(ncmp){ RegFieldDesc.reserved },
+    gang = Seq.fill(ncmp){ RegFieldDesc.reserved }
+  )
+
+  lazy val io = IO(new GenericTimerIO(regWidth, ncmp, maxcmp, scaleWidth, countWidth, cmpWidth))
+
 }
