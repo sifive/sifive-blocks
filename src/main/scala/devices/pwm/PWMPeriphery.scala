dcd95130c33b7240885490d48d55af2a2b7b7812
Henry Cook <henry@sifive.com>
2018-07-24 17:03:49 -0700

    pwm: converted to PeripheralPuncher

--- src/main/scala/devices/pwm/PWMPeriphery.scala
+++ src/main/scala/devices/pwm/PWMPeriphery.scala
@@ -3,39 +3,23 @@ package sifive.blocks.devices.pwm
 
 import Chisel._
 import freechips.rocketchip.config.Field
+import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.subsystem.BaseSubsystem
-import freechips.rocketchip.diplomacy.{LazyModule, LazyModuleImp}
-import freechips.rocketchip.util.HeterogeneousBag
-import sifive.blocks.devices.pinctrl.{Pin}
-
-class PWMPortIO(val c: PWMParams) extends Bundle {
-  val port = Vec(c.ncmp, Bool()).asOutput
-}
-
 
 case object PeripheryPWMKey extends Field[Seq[PWMParams]]
 
 trait HasPeripheryPWM { this: BaseSubsystem =>
-  val pwmParams = p(PeripheryPWMKey)
-  val pwms = pwmParams.zipWithIndex.map { case(params, i) =>
-    val name = Some(s"pwm_$i")
-    val pwm = LazyModule(new TLPWM(pbus.beatBytes, params)).suggestName(name)
-    pbus.toVariableWidthSlave(name) { pwm.node }
-    ibus.fromAsync := pwm.intnode
-    pwm
+  val pwms = p(PeripheryPWMKey).map { ps =>
+    PWM.attach(AttachedPWMParams(ps), pbus, ibus.fromSync, None)
   }
+  val pwmNodes = pwms.map(_.ioNode.makeSink())
 }
 
 trait HasPeripheryPWMBundle {
-  val pwm: HeterogeneousBag[PWMPortIO]
-
+  val pwm: Seq[PWMPortIO]
 }
 
 trait HasPeripheryPWMModuleImp extends LazyModuleImp with HasPeripheryPWMBundle {
   val outer: HasPeripheryPWM
-  val pwm = IO(HeterogeneousBag(outer.pwmParams.map(new PWMPortIO(_))))
-
-  (pwm zip outer.pwms) foreach { case (io, device) =>
-    io.port := device.module.io.gpio
-  }
+  val pwm = outer.pwmNodes.map(_.makeIO())
 }
