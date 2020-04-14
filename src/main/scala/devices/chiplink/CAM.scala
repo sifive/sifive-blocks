750070adba3df027f791f5a9bcf54b248248ee55
Wesley W. Terpstra <wesley@sifive.com>
2020-04-13 17:01:09 -0700

    chiplink: upgrade CAM to chisel3

--- src/main/scala/devices/chiplink/CAM.scala
+++ src/main/scala/devices/chiplink/CAM.scala
@@ -1,24 +1,25 @@
 // See LICENSE for license details.
 package sifive.blocks.devices.chiplink
 
-import Chisel._
+import chisel3._
+import chisel3.util._
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.util._
 
 class CAM(keys: Int, dataBits: Int) extends Module
 {
-  val io = new Bundle {
+  val io = IO(new Bundle {
     // alloc.valid => allocate a key
     // alloc.ready => a key is avilable
-    val alloc = Decoupled(UInt(width = dataBits)).flip
-    val key   = UInt(OUTPUT, width = log2Ceil(keys))
+    val alloc = Flipped(Decoupled(UInt(dataBits.W)))
+    val key   = Output(UInt(log2Ceil(keys).W))
     // free.valid => release the key
-    val free  = Valid(UInt(width = log2Ceil(keys))).flip
-    val data  = UInt(OUTPUT, width = dataBits)
-  }
+    val free  = Flipped(Valid(UInt(log2Ceil(keys).W)))
+    val data  = Output(UInt(dataBits.W))
+  })
 
-  val free = RegInit(UInt((BigInt(1) << keys) - 1, width = keys))
-  val data = Mem(keys, UInt(width = dataBits))
+  val free = RegInit(((BigInt(1) << keys) - 1).U(keys.W))
+  val data = Mem(keys, UInt(dataBits.W))
 
   val free_sel = ~(leftOR(free, keys) << 1) & free
   io.key := OHToUInt(free_sel, keys)
@@ -31,7 +32,7 @@ class CAM(keys: Int, dataBits: Int) extends Module
   io.data := Mux(bypass, io.alloc.bits, data(io.free.bits))
 
   // Update CAM usage
-  val clr = Mux(io.alloc.fire(), free_sel, UInt(0))
-  val set = Mux(io.free.valid, UIntToOH(io.free.bits), UInt(0))
+  val clr = Mux(io.alloc.fire(), free_sel, 0.U)
+  val set = Mux(io.free.valid, UIntToOH(io.free.bits), 0.U)
   free := (free & ~clr) | set
 }
