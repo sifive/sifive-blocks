1d5dfcfe3a89a12527dc7f9351015e2329303d82
Jade Kim <jade.kim@semifive.com>
2020-08-12 13:59:17 +0900

    Unify HasDFT trait
    HasDADft / HasJTAGDft -> HasDFT
    dftDANode / dftJTAGNode -> dftNode
    
    At the IP Onbrading
    	1. Mixin HasDFT Trait
    	2. Declare val dftNode as a bundle of ports to be used as DFT

--- src/main/scala/util/DFT.scala
+++ src/main/scala/util/DFT.scala
@@ -6,35 +6,22 @@ import chisel3._
 import freechips.rocketchip.config.{Parameters}
 import freechips.rocketchip.diplomacy._
 
-trait HasDADft[T <: Data] {
+trait HasDFT[T <: Data] {
   
-  def dftDANode: BundleBridgeSource[T]
+  def dftNode: BundleBridgeSource[T]
 
-  def makeDADftPort(implicit p: Parameters, valName: ValName): ModuleValue[T] = {
-    val dftSink = dftDANode.makeSink()
-    InModuleBody { dftSink.makeIO() } // will be change
-  }
-}
-
-trait HasJTAGDft[T <: Data] {
-  
-  def dftJTAGNode: BundleBridgeSource[T]
-
-  def makeJTAGDftPort(implicit p: Parameters): ModuleValue[T] = {
-    val dftSink = dftJTAGNode.makeSink()
+  def makePort(implicit p: Parameters): ModuleValue[T] = {
+    val dftSink = dftNode.makeSink()
     InModuleBody { dontTouch(dftSink.bundle) }
   }
 }
 
 trait CanHaveDFT { 
   
+  implicit val p: Parameters
+
   def devices: Seq[LazyModule]
 
-  val dftDA = devices.collect { case dft: HasDADft[Data] => dft }
-  val dftJTAG = devices.collect { case dft: HasJTAGDft[Data] => dft }
-  
-  def makeDftPort(implicit p: Parameters, valName: ValName) = {
-    dftDA.foreach(_.makeDADftPort)
-    dftJTAG.foreach(_.makeJTAGDftPort)
-  }
+  val dftNode = devices.collect { case source: HasDFT[Data] => source }
+  dftNode.foreach(_.makePort)
 }
