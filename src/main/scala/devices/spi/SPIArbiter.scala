7767b5a84a5e2d4d6aa89f2518e4ef99762f7d77
kritik bhimani <kritik.bhimani@sifive.com>
2020-08-12 15:06:31 +0530

    connect dummy in arbiter

--- src/main/scala/devices/spi/SPIArbiter.scala
+++ src/main/scala/devices/spi/SPIArbiter.scala
@@ -23,6 +23,7 @@ class SPIArbiter(c: SPIParamsBase, n: Int) extends Module {
   io.outer.fmt := Mux1H(sel, io.inner.map(_.fmt))
   // Workaround for overzealous combinational loop detection
   io.outer.cs := Mux(sel(0), io.inner(0).cs, io.inner(1).cs)
+  io.outer.dummy.foreach (_ := io.inner(0).dummy.get)
   require(n == 2, "SPIArbiter currently only supports 2 clients")
 
   (io.inner zip sel).foreach { case (inner, s) =>
