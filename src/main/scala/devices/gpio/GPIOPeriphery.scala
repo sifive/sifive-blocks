dad9768eff6191180b8e421f6d9b7c4ff9cdd3bd
Connor Lee <connor.lee@sifive.com>
2020-10-27 15:15:12 +0900

    GPIO: split function I/O bundle from GPIO bundle

--- src/main/scala/devices/gpio/GPIOPeriphery.scala
+++ src/main/scala/devices/gpio/GPIOPeriphery.scala
@@ -8,16 +8,19 @@ import freechips.rocketchip.subsystem.BaseSubsystem
 case object PeripheryGPIOKey extends Field[Seq[GPIOParams]](Nil)
 
 trait HasPeripheryGPIO { this: BaseSubsystem =>
-  val gpioNodes = p(PeripheryGPIOKey).map { ps =>
-    GPIOAttachParams(ps).attachTo(this).ioNode.makeSink()
-  }
+  val (gpioNodes, iofNodes) = p(PeripheryGPIOKey).map { ps =>
+    val gpio = GPIOAttachParams(ps).attachTo(this)
+    (gpio.ioNode.makeSink(), gpio.iofNode.map { _.makeSink() })
+  }.unzip
 }
 
 trait HasPeripheryGPIOBundle {
   val gpio: Seq[GPIOPortIO]
+  val iof: Seq[Option[IOFPortIO]]
 }
 
 trait HasPeripheryGPIOModuleImp extends LazyModuleImp with HasPeripheryGPIOBundle {
   val outer: HasPeripheryGPIO
   val gpio = outer.gpioNodes.zipWithIndex.map { case(n,i) => n.makeIO()(ValName(s"gpio_$i")) }
+  val iof = outer.iofNodes.zipWithIndex.map { case(o,i) => o.map { n => n.makeIO()(ValName(s"iof_$i")) } }
 }
